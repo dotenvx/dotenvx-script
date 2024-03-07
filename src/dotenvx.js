8db74b49e13382e9252186f60bf5054761233f1c
Scott Motte <mot@mot.la>
2024-03-06 17:29:58 -0800

    decrypted payload

--- src/dotenvx.js
+++ src/dotenvx.js
@@ -1,38 +1,135 @@
 class Dotenvx {
-  constructor(envVaultFile, envKeysFile) {
+  constructor(envVaultFile, envKeysFile, crypto = window.crypto) {
     this.envVaultFile = envVaultFile
     this.envKeysFile = envKeysFile
 
     this.LINE = /(?:^|^)\s*(?:export\s+)?([\w.-]+)(?:\s*=\s*?|:\s+?)(\s*'(?:\\'|[^'])*'|\s*"(?:\\"|[^"])*"|\s*`(?:\\`|[^`])*`|[^#\r\n]+)?\s*(?:#.*)?(?:$|$)/mg
+
+    this.crypto = crypto
   }
 
-  object () {
-    const obj = {}
+  async json (callback) {
+    const json = {}
     const environments = this.environments()
+    const dotenvKeys = this.dotenvKeys()
+    const dotenvVaults = this.dotenvVaults()
 
     for (const environment of environments) {
-      obj[environment] = {}
+      const dotenvKey = dotenvKeys[environment]
+      const encrypted = dotenvVaults[environment]
+
+      json[environment] = {}
+      json[environment]['dotenvKey'] = dotenvKey
+      json[environment]['encrypted'] = encrypted
+      try {
+        json[environment]['decrypted'] = await this.decrypt(encrypted, dotenvKey)
+      } catch (error) {
+        json[environment]['decrypted'] = null
+      }
     }
 
-    return obj
+    callback(json)
   }
 
-  environments() {
-    const obj = this._envVaultParsed()
-    const prefix = 'DOTENV_VAULT_'
-    const environments = []
-
+  dotenvKeys() {
+    const obj = this._envKeysParsed()
+    const prefix = 'DOTENV_KEY_'
+    const out = {}
     for (const key in obj) {
       if (obj.hasOwnProperty(key) && key.startsWith(prefix)) {
         const environment = key.replace(prefix, '').toLowerCase()
 
         if (environment) {
-          environments.push(environment)
+          out[environment] = obj[key] // dotenv://key...
         }
       }
     }
 
-    return environments
+    return out
+  }
+
+  dotenvVaults() {
+    const obj = this._envVaultParsed()
+    const prefix = 'DOTENV_VAULT_'
+    const out = {}
+    for (const key in obj) {
+      if (obj.hasOwnProperty(key) && key.startsWith(prefix)) {
+        const environment = key.replace(prefix, '').toLowerCase()
+
+        if (environment) {
+          out[environment] = obj[key] // encrypted
+        }
+      }
+    }
+
+    return out
+  }
+
+  environments() {
+    const obj = this.dotenvVaults()
+
+    return Object.keys(obj)
+  }
+
+  async decrypt(encrypted, dotenvKey) {
+    const environment = this._extractEnvironment(dotenvKey)
+    const decryptKey = this.decryptKey(dotenvKey)
+    const rawKey = this._fromHex(decryptKey)
+
+    const key = await this.crypto.subtle.importKey('raw', rawKey, { name: 'AES-GCM' }, false, ['decrypt'])
+
+    let ciphertext = this._fromBase64(encrypted)
+
+    // extract nonce and authtag
+    const nonce = ciphertext.slice(0, 12)
+    const authTag = ciphertext.slice(ciphertext.length - 16)
+    ciphertext = ciphertext.slice(12, ciphertext.length - 16)
+
+    // combine ciphertext and authtag
+    const data = new Uint8Array(ciphertext.length + authTag.length)
+    data.set(ciphertext)
+    data.set(authTag, ciphertext.length)
+
+    try {
+      const decrypted = await this.crypto.subtle.decrypt({ name: 'AES-GCM', iv: nonce }, key, data)
+
+      const decoder = new TextDecoder()
+      const plaintext = decoder.decode(decrypted)
+
+      return plaintext
+    } catch (error) {
+      console.error(`${environment}: decrypt() failed`, error)
+
+      throw error
+    }
+  }
+
+  // inputs a dotenvKey and returns the root decryptionKey portion
+  decryptKey (dotenvKey) {
+    // Parse DOTENV_KEY. Format is a URI
+    let uri
+    try {
+      uri = new URL(dotenvKey)
+    } catch (error) {
+      if (error.code === 'ERR_INVALID_URL') {
+        const err = new Error('INVALID_DOTENV_KEY: Wrong format. Must be in valid uri format like dotenv://:key_1234@dotenvx.com/vault/.env.vault?environment=development')
+        err.code = 'INVALID_DOTENV_KEY'
+        throw err
+      }
+
+      throw error
+    }
+
+    // Get decrypt key
+    const decryptKey = this._extractPassword(dotenvKey)
+    if (!decryptKey) {
+      const err = new Error('INVALID_DOTENV_KEY: Missing key part')
+      err.code = 'INVALID_DOTENV_KEY'
+      throw err
+    }
+
+    // get last 64
+    return decryptKey.slice(-64)
   }
 
   _envVaultParsed() {
@@ -81,4 +178,32 @@ class Dotenvx {
 
     return obj
   }
+
+  _extractPassword(uriString) {
+    const match = uriString.match(/:\/\/(?:[^:@]*:)?([^@]*)@/)
+
+    if (match) {
+      return match[1]
+    }
+
+    return null
+  }
+
+  _extractEnvironment(uriString) {
+    const uri = new URL(uriString)
+
+    return uri.searchParams.get('environment')
+  }
+
+  _fromHex(hexString) {
+    return new Uint8Array(hexString.match(/.{1,2}/g).map(byte => parseInt(byte, 16))) // convert key from hex string to buffer
+  }
+
+  _fromBase64(base64String) {
+    return Uint8Array.from(atob(base64String), c => c.charCodeAt(0))
+  }
+}
+
+if (typeof window === 'undefined') {
+  module.exports = Dotenvx
 }
--- tests/dotenvx.test.js
+++ tests/dotenvx.test.js
@@ -1,9 +1,8 @@
 const fs = require('fs')
 const vm = require('vm')
 const test = require('tape')
-
-const dotenvxContent = fs.readFileSync('src/dotenvx.js', 'utf-8')
-vm.runInThisContext(dotenvxContent)
+const crypto = require('crypto')
+const Dotenvx = require('../src/dotenvx.js')
 
 let envVaultFile = `
 #/-------------------.env.vault---------------------/
@@ -27,8 +26,22 @@ DOTENV_KEY_DEVELOPMENT="dotenv://:key_e507c60efa8841d8d5bbb85bd701ee92406cf3b065
 DOTENV_KEY_PRODUCTION="dotenv://:key_10283719af6a30ef49050048617f4fea10c23a38021fbebeb9fd858caa01852e@dotenvx.com/vault/.env.vault?environment=production"
 `
 
+test('#dotenvKeys', ct => {
+  const dotenvx = new Dotenvx(envVaultFile, envKeysFile, crypto)
+  const dotenvKeys = dotenvx.dotenvKeys()
+
+  const expected = {
+    development: 'dotenv://:key_e507c60efa8841d8d5bbb85bd701ee92406cf3b06506d1d80f1553c2a72791e4@dotenvx.com/vault/.env.vault?environment=development',
+    production: 'dotenv://:key_10283719af6a30ef49050048617f4fea10c23a38021fbebeb9fd858caa01852e@dotenvx.com/vault/.env.vault?environment=production'
+  }
+
+  ct.same(dotenvKeys, expected)
+
+  ct.end()
+})
+
 test('#environments', ct => {
-  const dotenvx = new Dotenvx(envVaultFile, envKeysFile)
+  const dotenvx = new Dotenvx(envVaultFile, envKeysFile, crypto)
   const environments = dotenvx.environments()
 
   ct.same(environments, ['development', 'production'])
@@ -36,16 +49,98 @@ test('#environments', ct => {
   ct.end()
 })
 
-test('#object', ct => {
-  const dotenvx = new Dotenvx(envVaultFile, envKeysFile)
-  const object = dotenvx.object()
+test('#decryptKey', ct => {
+  const dotenvKey = 'dotenv://:key_e507c60efa8841d8d5bbb85bd701ee92406cf3b06506d1d80f1553c2a72791e4@dotenvx.com/vault/.env.vault?environment=development'
+  const dotenvx = new Dotenvx(envVaultFile, envKeysFile, crypto)
+  const decryptKey = dotenvx.decryptKey(dotenvKey)
+
+  ct.same(decryptKey, 'e507c60efa8841d8d5bbb85bd701ee92406cf3b06506d1d80f1553c2a72791e4')
+
+  ct.end()
+})
+
+test('#decrypt', async ct => {
+  const dotenvKey = 'dotenv://:key_e507c60efa8841d8d5bbb85bd701ee92406cf3b06506d1d80f1553c2a72791e4@dotenvx.com/vault/.env.vault?environment=development'
+  const encrypted = 'V4NYVn0Pow6Uf2ez2mbHEzTrYURloHL6VDAFRLqnQBppA/OmHI5x5AXoxCMVor7wOg=='
+  const dotenvx = new Dotenvx(envVaultFile, envKeysFile, crypto)
+  const decryptKey = dotenvx.decrypt(encrypted, dotenvKey)
+
+  const expected = `# .env
+HELLO="World"
+`
+
+  const decrypted = await dotenvx.decrypt(encrypted, dotenvKey)
+
+  ct.same(decrypted, expected)
+  ct.end()
+})
+
+test('#json', ct => {
+  const dotenvx = new Dotenvx(envVaultFile, envKeysFile, crypto)
 
   const expected = {
-    development: {},
-    production: {}
+    development: {
+      dotenvKey: 'dotenv://:key_e507c60efa8841d8d5bbb85bd701ee92406cf3b06506d1d80f1553c2a72791e4@dotenvx.com/vault/.env.vault?environment=development',
+      encrypted: 'V4NYVn0Pow6Uf2ez2mbHEzTrYURloHL6VDAFRLqnQBppA/OmHI5x5AXoxCMVor7wOg==',
+      decrypted: '# .env\nHELLO="World"\n'
+    },
+    production: {
+      dotenvKey: 'dotenv://:key_10283719af6a30ef49050048617f4fea10c23a38021fbebeb9fd858caa01852e@dotenvx.com/vault/.env.vault?environment=production',
+      encrypted: 'YZkhtbh1IlzBgIamAAsG5nzGPfH6p8Zbuj9egXoziviVu/eYIyNjJWtIYyhiW/vHhFbqbsvo5+P9b27OC6ZC7qU=',
+      decrypted: '# .env.production\nHELLO="production"\n'
+    }
   }
 
-  ct.same(object, expected)
+  dotenvx.json(function(json) {
+    ct.same(json, expected)
+
+    ct.end()
+  })
+})
+
+test('#json (decryption fails)', ct => {
+  const dotenvx = new Dotenvx(envVaultFile, envKeysFile, crypto)
+
+  const expected = {
+    development: {
+      dotenvKey: 'dotenv://:key_e507c60efa8841d8d5bbb85bd701ee92406cf3b06506d1d80f1553c2a72791e4@dotenvx.com/vault/.env.vault?environment=development',
+      encrypted: 'V4NYVn0Pow6Uf2ez2mbHEzTrYURloHL6VDAFRLqnQBppA/OmHI5x5AXoxCMVor7wOg==',
+      decrypted: '# .env\nHELLO="World"\n'
+    },
+    production: {
+      dotenvKey: 'dotenv://:key_10283719af6a30ef49050048617f4fea10c23a38021fbebeb9fd858caa01852e@dotenvx.com/vault/.env.vault?environment=production',
+      encrypted: 'YZkhtbh1IlzBgIamAAsG5nzGPfH6p8Zbuj9egXoziviVu/eYIyNjJWtIYyhiW/vHhFbqbsvo5+P9b27OC6ZC7qU=',
+      decrypted: '# .env.production\nHELLO="production"\n'
+    }
+  }
+
+  dotenvx.json(function(json) {
+    ct.same(json, expected)
+
+    ct.end()
+  })
+})
+
+test('#_extractPassword', ct => {
+  const dotenvKey = 'dotenv://:key_e507c60efa8841d8d5bbb85bd701ee92406cf3b06506d1d80f1553c2a72791e4@dotenvx.com/vault/.env.vault?environment=development'
+
+  const dotenvx = new Dotenvx(envVaultFile, envKeysFile, crypto)
+
+  const password = dotenvx._extractPassword(dotenvKey)
+
+  ct.same(password, 'key_e507c60efa8841d8d5bbb85bd701ee92406cf3b06506d1d80f1553c2a72791e4')
+
+  ct.end()
+})
+
+test('#_extractEnvironment', ct => {
+  const dotenvKey = 'dotenv://:key_e507c60efa8841d8d5bbb85bd701ee92406cf3b06506d1d80f1553c2a72791e4@dotenvx.com/vault/.env.vault?environment=development'
+
+  const dotenvx = new Dotenvx(envVaultFile, envKeysFile, crypto)
+
+  const password = dotenvx._extractEnvironment(dotenvKey)
+
+  ct.same(password, 'development')
 
   ct.end()
 })
